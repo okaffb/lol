0e8596c4eee3dd36249e6331afed053b1d2c5baf
okaffb <vikingrafflesia33@gmail.com>
2021-01-09 14:58:44 +0700

    first commit

--- lolMiner_v1.18a_Lin64/1.18a/emergency.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-##################################
-## Emergency script in case the ##
-## miner crashes. User defined  ##
-## actions to be inserted here  ##
-##################################
-
-echo "Hallo World from emergency script"
